18d5e1ec5b25d26e8b037ea51c78beb21f8657f4
LTNhoTin <tinle155@gmail.com>
2025-11-11 23:45:39 +0700

    ok

--- deep_q_network.py
+++ deep_q_network.py
@@ -35,14 +35,15 @@ EXPLORE = 2000000.  # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001  # final value of epsilon
 INITIAL_EPSILON = 0.0001  # starting value of epsilon
 REPLAY_MEMORY = 50000  # number of previous transitions to remember
-BATCH = 128  # size of minibatch
+BATCH = 512  # size of minibatch
 FRAME_PER_ACTION = 1
 
 # Prepare dirs
 CHECKPOINT_DIR = "saved_networks"
 LOG_FILE = "log.txt"
 VIDEO_DIR = "videos"
-VIDEO_PATH = os.path.join(VIDEO_DIR, "train.mp4")
+VIDEO_TEMPLATE = os.path.join(VIDEO_DIR, "train_step_{step:07d}.mp4")
+VIDEO_FPS = 60.0
 
 os.makedirs(CHECKPOINT_DIR, exist_ok=True)
 os.makedirs("logs_" + GAME, exist_ok=True)
@@ -136,7 +137,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
     # saving and loading networks
-    saver = tf.compat.v1.train.Saver()
+    saver = tf.compat.v1.train.Saver(max_to_keep=None)
     sess.run(tf.compat.v1.global_variables_initializer())
     checkpoint = tf.compat.v1.train.get_checkpoint_state(CHECKPOINT_DIR)
     if checkpoint and checkpoint.model_checkpoint_path:
@@ -150,114 +151,143 @@ def trainNetwork(s, readout, h_fc1, sess):
 
     # setup video writer
     video_writer = None
+    current_video_step = None
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+    def start_video_writer(step, frame):
+        nonlocal video_writer, current_video_step
+        if video_writer is not None:
+            return
+        try:
+            height, width = frame.shape[:2]
+            video_path = VIDEO_TEMPLATE.format(step=step)
+            video_writer = cv2.VideoWriter(video_path, fourcc, VIDEO_FPS, (width, height))
+            current_video_step = step
+            logging.info("Started recording video segment: %s", video_path)
+        except Exception as e:
+            logging.warning("Video writer init failed: %s", str(e))
+            video_writer = None
+            current_video_step = None
+
+    def close_video_writer():
+        nonlocal video_writer, current_video_step
+        if video_writer is None:
+            return
+        try:
+            video_writer.release()
+            if current_video_step is not None:
+                logging.info("Closed video segment for step %s", current_video_step)
+        except Exception as e:
+            logging.warning("Video writer release failed: %s", str(e))
+        finally:
+            video_writer = None
+            current_video_step = None
 
     # start training
     epsilon = INITIAL_EPSILON
     t = 0
-    while "flappy bird" != "angry bird":
-        # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s: [s_t]})[0]
-        a_t = np.zeros([ACTIONS])
-        action_index = 0
-        if t % FRAME_PER_ACTION == 0:
-            if random.random() <= epsilon:
-                logging.info("----------Random Action----------")
-                action_index = random.randrange(ACTIONS)
-                a_t[random.randrange(ACTIONS)] = 1
-            else:
-                action_index = np.argmax(readout_t)
-                a_t[action_index] = 1
-        else:
-            a_t[0] = 1 # do nothing
-
-        # scale down epsilon
-        if epsilon > FINAL_EPSILON and t > OBSERVE:
-            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
-
-        # run the selected action and observe next state and reward
-        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
-        # init video writer lazily after we know frame size
-        if video_writer is None:
-            try:
-                height = x_t1_colored.shape[1]
-                width = x_t1_colored.shape[0]
-                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                video_writer = cv2.VideoWriter(VIDEO_PATH, fourcc, 60.0, (width, height))
-            except Exception as e:
-                logging.warning("Video writer init failed: %s", str(e))
-        x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
-        ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
-        x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
-        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
-
-        # write video frame (convert RGB -> BGR for OpenCV)
-        if video_writer is not None:
-            try:
-                bgr = cv2.cvtColor(x_t1_colored, cv2.COLOR_RGB2BGR)
-                video_writer.write(bgr)
-            except Exception as e:
-                logging.warning("Video write failed: %s", str(e))
-
-        # store the transition in D
-        D.append((s_t, a_t, r_t, s_t1, terminal))
-        if len(D) > REPLAY_MEMORY:
-            D.popleft()
-
-        # only train if done observing
-        if t > OBSERVE:
-            # sample a minibatch to train on
-            minibatch = random.sample(D, BATCH)
-
-            # get the batch variables
-            s_j_batch = [d[0] for d in minibatch]
-            a_batch = [d[1] for d in minibatch]
-            r_batch = [d[2] for d in minibatch]
-            s_j1_batch = [d[3] for d in minibatch]
-
-            y_batch = []
-            readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
-            for i in range(0, len(minibatch)):
-                terminal = minibatch[i][4]
-                # if terminal, only equals reward
-                if terminal:
-                    y_batch.append(r_batch[i])
+    try:
+        while "flappy bird" != "angry bird":
+            # choose an action epsilon greedily
+            readout_t = readout.eval(feed_dict={s: [s_t]})[0]
+            a_t = np.zeros([ACTIONS])
+            action_index = 0
+            if t % FRAME_PER_ACTION == 0:
+                if random.random() <= epsilon:
+                    logging.info("----------Random Action----------")
+                    action_index = random.randrange(ACTIONS)
+                    a_t[random.randrange(ACTIONS)] = 1
                 else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                    action_index = np.argmax(readout_t)
+                    a_t[action_index] = 1
+            else:
+                a_t[0] = 1 # do nothing
 
-            # perform gradient step
-            train_step.run(feed_dict={
-                y: y_batch,
-                a: a_batch,
-                s: s_j_batch}
-            )
+            # scale down epsilon
+            if epsilon > FINAL_EPSILON and t > OBSERVE:
+                epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
-        # update the old values
-        s_t = s_t1
-        t += 1
+            # run the selected action and observe next state and reward
+            x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+            # init video writer lazily after we know frame size
+            if video_writer is None:
+                start_video_writer(t, x_t1_colored)
+            x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
+            ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+            x_t1 = np.reshape(x_t1, (80, 80, 1))
+            #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+            s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
-        # save progress every 10000 iterations
-        if t % 10000 == 0:
-            saver.save(sess, os.path.join(CHECKPOINT_DIR, GAME + '-dqn'), global_step=t)
+            # write video frame (convert RGB -> BGR for OpenCV)
+            if video_writer is not None:
+                try:
+                    bgr = cv2.cvtColor(x_t1_colored, cv2.COLOR_RGB2BGR)
+                    video_writer.write(bgr)
+                except Exception as e:
+                    logging.warning("Video write failed: %s", str(e))
 
-        # print info
-        state = ""
-        if t <= OBSERVE:
-            state = "observe"
-        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
-            state = "explore"
-        else:
-            state = "train"
+            # store the transition in D
+            D.append((s_t, a_t, r_t, s_t1, terminal))
+            if len(D) > REPLAY_MEMORY:
+                D.popleft()
 
-        logging.info("TIMESTEP %d / STATE %s / EPSILON %.6f / ACTION %d / REWARD %.4f / Q_MAX %e",
-                     t, state, epsilon, action_index, r_t, np.max(readout_t))
-        # write info to files
-        '''
-        if t % 10000 <= 100:
-            a_file.write(",".join([str(x) for x in readout_t]) + '\n')
-            h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
-            cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
-        '''
+            # only train if done observing
+            if t > OBSERVE:
+                # sample a minibatch to train on
+                minibatch = random.sample(D, BATCH)
+
+                # get the batch variables
+                s_j_batch = [d[0] for d in minibatch]
+                a_batch = [d[1] for d in minibatch]
+                r_batch = [d[2] for d in minibatch]
+                s_j1_batch = [d[3] for d in minibatch]
+
+                y_batch = []
+                readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
+                for i in range(0, len(minibatch)):
+                    terminal = minibatch[i][4]
+                    # if terminal, only equals reward
+                    if terminal:
+                        y_batch.append(r_batch[i])
+                    else:
+                        y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+
+                # perform gradient step
+                train_step.run(feed_dict={
+                    y: y_batch,
+                    a: a_batch,
+                    s: s_j_batch}
+                )
+
+            # update the old values
+            s_t = s_t1
+            t += 1
+
+            # save progress every 10000 iterations
+            if t % 10000 == 0:
+                saver.save(sess, os.path.join(CHECKPOINT_DIR, GAME + '-dqn'), global_step=t)
+                close_video_writer()
+
+            # print info
+            state = ""
+            if t <= OBSERVE:
+                state = "observe"
+            elif t > OBSERVE and t <= OBSERVE + EXPLORE:
+                state = "explore"
+            else:
+                state = "train"
+
+            logging.info("TIMESTEP %d / STATE %s / EPSILON %.6f / ACTION %d / REWARD %.4f / Q_MAX %e",
+                         t, state, epsilon, action_index, r_t, np.max(readout_t))
+            # write info to files
+            '''
+            if t % 10000 <= 100:
+                a_file.write(",".join([str(x) for x in readout_t]) + '\n')
+                h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
+                cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
+            '''
+    finally:
+        close_video_writer()
 
 def playGame():
     sess = tf.compat.v1.InteractiveSession()
